00c1f9d16d6c90bb60df945635ea003b7bd05a2b
haruki1953 <kinikun@outlook.com>
2024-11-03 20:42:16 +0800

    RSS实现 姑且完成了，先部署，明天再看

--- py-test.py
+++ py-test.py
@@ -46,76 +46,6 @@ def update_info_to_bgm_info_link(update_info):
     # 获取前三个路径部分 ['', 'Bangumi', '魔法光源股份有限公司'] ，然后拼接
     return '/'.join(watchlink.split('/')[:3])
 
-def get_input_data():
-    """
-    获取传入的数据
-    从参数获取数据，返回一个字典
-    """
-    torrent_name = sys.argv[1]
-    content_dir = sys.argv[2]
-    root_dir = sys.argv[3]
-    save_dir = sys.argv[4]
-    files_num = sys.argv[5]
-    torrent_size = sys.argv[6]
-    file_hash = sys.argv[7]
-    file_date = datetime.now().isoformat()
-    
-    return {
-        "fileName": torrent_name,
-        "filePath": save_dir,
-        "fileSize": int(torrent_size),
-        "fileHash": file_hash,
-        "fileDate": file_date
-    }
-
-def read_update_json():
-    """
-    从update_json_file中读取数据
-    返回数据
-    """
-    try:
-        with open(update_json_file, 'r', encoding='utf-8') as file:
-            return json.load(file)
-    except:
-        return []
-
-def process_data(update_data, new_data):
-    """
-    处理数据
-    在update_json中删除和当前filePath一样的
-    将当前数据插入数组首部
-    将长度限制update_limit_length（删除尾部）
-    返回处理后的数据
-    """
-    update_data = [item for item in update_data if item['filePath'] != new_data['filePath']]
-    update_data.insert(0, new_data)
-    return update_data[:update_limit_length]
-
-def save_update_json(data):
-    """
-    保存数据
-    将数据保存回update_json_file
-    """
-    with open(update_json_file, 'w', encoding='utf-8') as file:
-        # 使用 separators 参数生成紧凑的 JSON 文件
-        json.dump(data, file, ensure_ascii=False, separators=(',', ':'))
-
-def update_config_json():
-    """
-    修改config_json_file
-    将bgmLastUpdate字段保存当前时间
-    """
-    try:
-        with open(config_json_file, 'r', encoding='utf-8') as file:
-            config_data = json.load(file)
-    except:
-        config_data = {}
-
-    config_data['bgmLastUpdate'] = datetime.now().isoformat()
-
-    with open(config_json_file, 'w', encoding='utf-8') as file:
-        # 保存为便于查看的格式 indent=4 指定每一级缩进使用4个空格
-        json.dump(config_data, file, ensure_ascii=False, indent=4)
 
 def generate_rss(anime_updates):
     # 创建根节点
@@ -163,18 +93,30 @@ def generate_rss(anime_updates):
     with open(rss_xml_file, "w", encoding="utf-8") as f:
         f.write(xml_str)
 
+# 示例番剧更新数组
+anime_updates_example = [
+    {
+        "fileName": "[ANi] BLEACH 死神 千年血戰篇-相剋譚- - 31 [1080P][Baha][WEB-DL][AAC AVC][CHT].mp4",
+        "filePath": "/root/Downloads/Sakiko/Bangumi/死神 千年血战篇/Season 1",
+        "fileSize": 564933851,
+        "fileHash": "d4db5e25ef494015264cb7c738c1c9510fd0f5a2",
+        "fileDate": "2024-11-02T23:44:20.560360"
+    },
+    {
+        "fileName": "[Nekomoe kissaten][Ao no Hako][06][1080p][JPSC].mp4",
+        "filePath": "/root/Downloads/Sakiko/Bangumi/青之箱/Season 1",
+        "fileSize": 490813890,
+        "fileHash": "73ef5421c42f70bb519c35de08641bc4cb1f4bef",
+        "fileDate": "2024-11-02T17:12:19.054477"
+    },
+    {
+        "fileName": "[ANi] 蜻蛉高球 - 18 [1080P][Baha][WEB-DL][AAC AVC][CHT].mp4",
+        "filePath": "/root/Downloads/Sakiko/Bangumi/喂！蜻蜓/Season 1",
+        "fileSize": 302695972,
+        "fileHash": "b256eeab78955be10695ebd11d5119fb72b2060d",
+        "fileDate": "2024-11-02T09:39:46.145724"
+    },
+]
 
-def main():
-    # 获取传入的数据
-    new_data = get_input_data()
-    # 从update_json_file中读取数据
-    update_data = read_update_json()
-    # 处理数据
-    processed_data = process_data(update_data, new_data)
-    # 保存数据
-    save_update_json(processed_data)
-    # 修改config_json_file
-    update_config_json()
-
-if __name__ == "__main__":
-    main()
+# 调用函数生成RSS文件
+generate_rss(anime_updates_example)
